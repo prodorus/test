944b3ce2e7980fbb512d71d1dc6b0c9fffddb0fe
prodorus <106146774+prodorus@users.noreply.github.com>
2022-06-08 19:29:34 +0300

    test26

--- src/io/libs/ProjectHelpers.groovy
+++ src/io/libs/ProjectHelpers.groovy
@@ -119,21 +119,10 @@ def dropDb(server1c, agentPort, serverSql, base, admin1cUser, admin1cPwd, sqluse
 // Параметры:
 //
 //
-def loadCfgFrom1CStorage(storageTCP, storageUser, storagePwd, connString, admin1cUser, admin1cPassword, platform) {
+def loadCfgFrom1CStorage(connString, admin1cUser, admin1cPassword, platform) {
     utils = new Utils()
 
-    storagePwdLine = ""
-    if (storagePwd != null && !storagePwd.isEmpty()) {
-        storagePwdLine = "--storage-pwd ${storagePwd}"
-    }
-
-    platformLine = ""
-    if (platform != null && !platform.isEmpty()) {
-        platformLine = "--v8version ${platform}"
-    }
-
-
-    returnCode = utils.cmd("git clone https://github.com/prodorus/1cconf1")
+    returnCode = utils.cmd("git pull https://github.com/prodorus/1cconf1")
     if (returnCode != 0) {
          utils.raiseError("Загрузка конфигурации из 1С хранилища  ${storageTCP} завершилась с ошибкой. Для подробностей смотрите логи.")
     }
